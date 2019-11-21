3dc2315f24af5baca2696a93468ecf40027f01f9
Eduardo Minguez Perez <e.minguez@gmail.com>
2019-11-21 17:14:08 +0100

    Avoid requiring skopeo installed

--- hack/env.sh
+++ hack/env.sh
@@ -4,6 +4,12 @@ OPERATOR_EXEC=oc
 
 export RELEASE_VERSION=v4.3.0
 export OPERATOR_NAME=ptp-operator
+IMAGE_REPOSITORY="https://quay.io/api/v1/repository/openshift/origin-ptp"
 
-LINUXPTP_DAEMON_IMAGE_DIGEST=$(skopeo inspect docker://quay.io/openshift/origin-ptp | jq --raw-output '.Digest')
-export LINUXPTP_DAEMON_IMAGE=${LINUXPTP_DAEMON_IMAGE:-quay.io/openshift/origin-ptp@${LINUX_DAEMON_IMAGE_DIGEST}}
+if [ ! -x "$(command -v jq)" ]; then
+  echo "Could not find jq in the current PATH"
+  exit 1
+fi
+
+LINUXPTP_DAEMON_IMAGE_DIGEST=$(curl -s -X GET "${IMAGE_REPOSITORY}/tag/?specificTag=${RELEASE_VERSION//v}&onlyActiveTags=true" | jq -r .tags[0].manifest_digest)
+export LINUXPTP_DAEMON_IMAGE=${LINUXPTP_DAEMON_IMAGE:-quay.io/openshift/origin-ptp@${LINUXPTP_DAEMON_IMAGE_DIGEST}}
