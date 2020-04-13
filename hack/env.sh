aa3e6664285fbc6cb99dddf0f265ee8b8976c2ae
Sebastian Sch <sebassch@gmail.com>
2020-04-13 22:56:51 +0300

    Fix 4.3 env file for the test deployment
    Signed-off-by: Sebastian Sch <sebassch@gmail.com>

--- hack/env.sh
+++ hack/env.sh
@@ -3,7 +3,11 @@ NAMESPACE=openshift-ptp
 OPERATOR_EXEC=oc
 
 export RELEASE_VERSION=v4.3.0
+export IMAGE_TAG=4.3
 export OPERATOR_NAME=ptp-operator
 
+# This will find the latest digest.
 LINUXPTP_DAEMON_IMAGE_DIGEST=$(skopeo inspect docker://quay.io/openshift/origin-ptp | jq --raw-output '.Digest')
-export LINUXPTP_DAEMON_IMAGE=${LINUXPTP_DAEMON_IMAGE:-quay.io/openshift/origin-ptp@${LINUX_DAEMON_IMAGE_DIGEST}}
+PTP_OPERATOR_IMAGE_DIGEST=$(skopeo inspect docker://quay.io/openshift/origin-ptp-operator | jq --raw-output '.Digest')
+export LINUXPTP_DAEMON_IMAGE=${LINUXPTP_DAEMON_IMAGE:-quay.io/openshift/origin-ptp:${IMAGE_TAG}}
+export PTP_OPERATOR_IMAGE=${PTP_OPERATOR_IMAGE:-quay.io/openshift/origin-ptp-operator:${IMAGE_TAG}}
