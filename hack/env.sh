acde5b09ef0cf468aee31eafac6692de377a2ab1
Sebastian Sch <sebassch@gmail.com>
2020-04-13 22:54:19 +0300

    Fix env file for the operator test deployment
    Signed-off-by: Sebastian Sch <sebassch@gmail.com>

--- hack/env.sh
+++ hack/env.sh
@@ -2,10 +2,12 @@ REPO_DIR="$(dirname $0)/.."
 NAMESPACE=openshift-ptp
 OPERATOR_EXEC=oc
 
-export RELEASE_VERSION=v4.3.0
+export RELEASE_VERSION=v4.4.0
+export IMAGE_TAG=4.4
 export OPERATOR_NAME=ptp-operator
 
+# This will find the latest digest.
 LINUXPTP_DAEMON_IMAGE_DIGEST=$(skopeo inspect docker://quay.io/openshift/origin-ptp | jq --raw-output '.Digest')
 PTP_OPERATOR_IMAGE_DIGEST=$(skopeo inspect docker://quay.io/openshift/origin-ptp-operator | jq --raw-output '.Digest')
-export LINUXPTP_DAEMON_IMAGE=${LINUXPTP_DAEMON_IMAGE:-quay.io/openshift/origin-ptp@${LINUXPTP_DAEMON_IMAGE_DIGEST}}
-export PTP_OPERATOR_IMAGE=${PTP_OPERATOR_IMAGE:-quay.io/openshift/origin-ptp-operator@${PTP_OPERATOR_IMAGE_DIGEST}}
+export LINUXPTP_DAEMON_IMAGE=${LINUXPTP_DAEMON_IMAGE:-quay.io/openshift/origin-ptp:${IMAGE_TAG}}
+export PTP_OPERATOR_IMAGE=${PTP_OPERATOR_IMAGE:-quay.io/openshift/origin-ptp-operator:${IMAGE_TAG}}
