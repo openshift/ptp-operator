e5fb59c9556f7c1aecd1528570abb50e8da0c81f
Gonzalo Reyero Ferreras <greyerof@redhat.com>
2022-12-14 17:53:49 +0000

    CPU utilization test case for the parallel test suite.
    The cpu usage is measured once per minute until the TC's configured
    timeout is reached or the failure threshold is reached, whatever comes
    first. Every cpu check cycle, the cpu usage is retrieved for each
    ptp-daemon and ptp-operator pods. In case any pod (no matter how many)
    has a cpu usage higher than the limit, the failure counter is increased
    by one.
    
    K8s' PodMetrics is used to retrieve the ptp pods cpu usage in milliCores.
    The k8s metrics client has been added to the go-client Client struct.

--- test/conformance/parallel/ptp.go
+++ test/conformance/parallel/ptp.go
@@ -16,17 +16,16 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	v1 "k8s.io/api/core/v1"
 
-	v1core "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
+	ptptestconfig "github.com/openshift/ptp-operator/test/conformance/config"
 	"github.com/openshift/ptp-operator/test/pkg"
 	"github.com/openshift/ptp-operator/test/pkg/client"
 	testclient "github.com/openshift/ptp-operator/test/pkg/client"
 	"github.com/openshift/ptp-operator/test/pkg/execute"
 	"github.com/openshift/ptp-operator/test/pkg/pods"
+	"github.com/openshift/ptp-operator/test/pkg/ptptesthelper"
 	"github.com/openshift/ptp-operator/test/pkg/testconfig"
-
-	ptptestconfig "github.com/openshift/ptp-operator/test/conformance/config"
+	v1core "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	. "github.com/onsi/gomega"
 	"github.com/sirupsen/logrus"
@@ -49,7 +48,7 @@ var _ = Describe("[ptp-long-running]", func() {
 				Skip("Failed to find a valid ptp slave configuration")
 			}
 
-			ptpPods, err := client.Client.CoreV1().Pods(pkg.PtpLinuxDaemonNamespace).List(context.Background(), metav1.ListOptions{LabelSelector: "app=linuxptp-daemon"})
+			ptpPods, err := client.Client.CoreV1().Pods(pkg.PtpLinuxDaemonNamespace).List(context.Background(), metav1.ListOptions{LabelSelector: pkg.PtpLinuxDaemonPodsLabel})
 			Expect(err).NotTo(HaveOccurred())
 			Expect(len(ptpPods.Items)).To(BeNumerically(">", 0), "linuxptp-daemon is not deployed on cluster")
 
@@ -77,6 +76,10 @@ var _ = Describe("[ptp-long-running]", func() {
 
 		It("PTP Slave Clock Sync", func() {
 			testPtpSlaveClockSync(fullConfig, testParameters) // Implementation of the test case
+		})
+
+		It("PTP CPU Utilization", func() {
+			testPtpCpuUtilization(fullConfig, testParameters)
 		})
 	})
 })
@@ -111,6 +114,78 @@ func testPtpSlaveClockSync(fullConfig testconfig.TestConfig, testParameters ptpt
 	}
 	logrus.Info("Failure threshold = ", failureThreshold)
 	// Actual implementation
+}
+
+// This test will run for configured minutes or until failure_threshold reached,
+// whatever comes first. A failure_threshold is reached each time the cpu usage
+// of any of the ptp pods (daemonset & operator) is higher than the expected one,
+// which is hardcoded to 15milliCores. The cpu usage is retrieved once per minute.
+func testPtpCpuUtilization(fullConfig testconfig.TestConfig, testParameters ptptestconfig.PtpTestConfig) {
+	const (
+		minimumFailureThreshold  = 1
+		cpuUsageCheckingInterval = 1 * time.Minute
+		milliCoresThreshold      = ptptestconfig.PtpDefaultMilliCoresUsageThreshold
+	)
+
+	logrus.Debugf("CPU Utilization TC Config: %+v", testParameters.SoakTestConfig.CpuUtilization)
+
+	params := testParameters.SoakTestConfig.CpuUtilization.TestSpec
+	if !params.Enable {
+		Skip("skip the test - the test is disabled")
+		return
+	}
+
+	// Create timer channel for test case timeout.
+	testCaseDuration := time.Duration(params.Duration) * time.Minute
+	tcEndChan := time.After(testCaseDuration)
+
+	// Create ticker for cpu usage checker function.
+	cpuUsageCheckTicker := time.NewTicker(cpuUsageCheckingInterval)
+
+	// Set failureThresold limit number.
+	failureThreshold := minimumFailureThreshold
+	if params.FailureThreshold > minimumFailureThreshold {
+		failureThreshold = params.FailureThreshold
+	}
+
+	logrus.Infof("Running TC for %s (failure threshold: %d)", testCaseDuration.String(), failureThreshold)
+
+	failureCounter := 0
+	for {
+		select {
+		case <-tcEndChan:
+			// TC ended: report & return.
+			logrus.Infof("CPU utilization threshold reached %d times.", failureCounter)
+			return
+		case <-cpuUsageCheckTicker.C:
+			logrus.Infof("Retrieving cpu usage of the ptp pods.")
+
+			thresholdReached, err := isCpuUsageThresholdReachedInPtpPods(milliCoresThreshold)
+			Expect(err).To(BeNil(), "failed to get cpu usage: ", err)
+
+			if thresholdReached {
+				failureCounter++
+				Expect(failureCounter).To(BeNumerically("<", failureThreshold),
+					fmt.Sprintf("Failure threshold (%d) reached", failureThreshold))
+			}
+		}
+	}
+}
+
+// isCpuUsageThresholdReachedInPtpPods is a helper that checks whether the usage on each
+// ptp-daemonset's pods is lower than a given threshold in milliCores.
+func isCpuUsageThresholdReachedInPtpPods(milliCoresThreshold int64) (bool, error) {
+	daemonsetPodsCpuUsageThresholdReached, err := ptptesthelper.IsPtpDaemonPodsCpuUsageHigherThan(milliCoresThreshold)
+	if err != nil {
+		return false, fmt.Errorf("failed to get linux-ptp daemonset pods cpu usage: %v", err)
+	}
+
+	operatorPodsCpuUsageThresholdReached, err := ptptesthelper.IsPtpOperatorPodsCpuUsageHigherThan(milliCoresThreshold)
+	if err != nil {
+		return false, fmt.Errorf("failed to get ptp operator pods cpu usage: %v", err)
+	}
+
+	return (daemonsetPodsCpuUsageThresholdReached || operatorPodsCpuUsageThresholdReached), nil
 }
 
 func GetPodLogs(namespace, podName, containerName string, min, max int, messages chan string, ctx context.Context) {
