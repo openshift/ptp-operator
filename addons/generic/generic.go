e426db7999e412ab802b105c3c6d54392cbe3e0d
Joseph Richard <joseph.richard@redhat.com>
2023-06-27 22:20:19 -0600

    Add generic plugin logging

--- addons/generic/generic.go
+++ addons/generic/generic.go
@@ -1,22 +1,72 @@
 package generic
 
 import (
+	"encoding/json"
 	"github.com/golang/glog"
 	ptpv1 "github.com/k8snetworkplumbingwg/ptp-operator/api/v1"
 	"github.com/k8snetworkplumbingwg/ptp-operator/pkg/daemon/plugin"
 )
 
-func onPTPConfigChangeGeneric(*interface{}, *ptpv1.PtpProfile) error {
-	glog.Infof("calling onPTPConfigChangeGeneric")
+type GenericPluginData struct {
+	referenceString *string
+}
+
+func OnPTPConfigChangeGeneric(data *interface{}, nodeProfile *ptpv1.PtpProfile) error {
+	var pluginOpts string = ""
+	var err error
+	var optsByteArray []byte
+
+	for name, opts := range (*nodeProfile).Plugins {
+		if name == "reference" {
+			optsByteArray, _ = json.Marshal(opts)
+			err = json.Unmarshal(optsByteArray, &pluginOpts)
+		}
+	}
+
+	if data != nil && pluginOpts != "" {
+		_data := *data
+		glog.Infof("Saving status to hwconfig: %s", string(pluginOpts))
+		var pluginData *GenericPluginData = _data.(*GenericPluginData)
+		_pluginData := *pluginData
+		*_pluginData.referenceString = pluginOpts
+	}
+	glog.Infof("OnPTPConfigChangeGeneric: (%s)", pluginOpts)
+	return err
+
+}
+
+func PopulateHwConfigGeneric(data *interface{}, hwconfigs *[]ptpv1.HwConfig) error {
+	glog.Infof("PopulateHwConfigGeneric")
+	var referenceString string = ""
+	if data != nil {
+		_data := *data
+		var pluginData *GenericPluginData = _data.(*GenericPluginData)
+		_pluginData := *pluginData
+		if _pluginData.referenceString != nil {
+			referenceString = *_pluginData.referenceString
+			hwConfig := ptpv1.HwConfig{}
+			hwConfig.DeviceID = "reference"
+			hwConfig.Status = referenceString
+			*hwconfigs = append(*hwconfigs, hwConfig)
+		}
+	}
+	glog.Infof("PopulateHwConfigGeneric: (%s)", referenceString)
 	return nil
 }
 
-func PopulateHwConfigGeneric(*interface{}, *[]ptpv1.HwConfig) error {
-	return nil
-}
+func AfterRunPTPCommandGeneric(data *interface{}, nodeProfile *ptpv1.PtpProfile, command string) error {
+	var pluginOpts string = ""
+	var err error
+	var optsByteArray []byte
 
-func AfterRunPTPCommandGeneric(*interface{}, *ptpv1.PtpProfile, string) error {
-	return nil
+	for name, opts := range (*nodeProfile).Plugins {
+		if name == "reference" {
+			optsByteArray, _ = json.Marshal(opts)
+			err = json.Unmarshal(optsByteArray, &pluginOpts)
+		}
+	}
+	glog.Infof("AfterRunPTPCommandGeneric: (%s,%s)", command, pluginOpts)
+	return err
 }
 
 func Reference(name string) (*plugin.Plugin, *interface{}) {
@@ -25,9 +75,12 @@ func Reference(name string) (*plugin.Plugin, *interface{}) {
 		return nil, nil
 	}
 	_plugin := plugin.Plugin{Name: "reference",
-		OnPTPConfigChange:  onPTPConfigChangeGeneric,
+		OnPTPConfigChange:  OnPTPConfigChangeGeneric,
 		PopulateHwConfig:   PopulateHwConfigGeneric,
 		AfterRunPTPCommand: AfterRunPTPCommandGeneric,
 	}
-	return &_plugin, nil
+	var referenceString string = ""
+	pluginData := GenericPluginData{referenceString: &referenceString}
+	var iface interface{} = &pluginData
+	return &_plugin, &iface
 }
