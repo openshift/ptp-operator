a3c42baa9185fb69cb8814c1655b69312ab9c6e4
Jack Ding <jackding@gmail.com>
2024-02-28 17:05:54 -0500

    fix CPU Utilization soak test
    Remove trial prometheus queries.
    
    Check container cpu usage only if a threshold target for this container is set
    in the test config. This reduces the number of prometheus queries significantly
    and avoid queries for kube-rbac-proxy and pause containers.
    
    When retry prometheus queries, increase the rateTimeWindow by 10 seconds with
    each retry to avoid returning empty data due to not enough samples being
    captured within the window.
    
    Signed-off-by: Jack Ding <jackding@gmail.com>

--- test/conformance/parallel/ptp.go
+++ test/conformance/parallel/ptp.go
@@ -64,7 +64,6 @@ var _ = Describe("["+strings.ToLower(DesiredMode.String())+"-parallel]", func() 
 
 	Context("Event based tests", func() {
 		BeforeEach(func() {
-
 			if !ptphelper.PtpEventEnabled() {
 				Skip("Skipping, PTP events not enabled")
 			}
@@ -75,7 +74,6 @@ var _ = Describe("["+strings.ToLower(DesiredMode.String())+"-parallel]", func() 
 		})
 
 		It("PTP Slave Clock Sync", func() {
-
 			testPtpSlaveClockSync(fullConfig, testParameters) // Implementation of the test case
 
 		})
@@ -246,17 +244,16 @@ func isCpuUsageThresholdReachedInPtpPods(prometheusPod *v1core.Pod, ptpPodsPerNo
 
 			for i := range pod.Spec.Containers {
 				container := &pod.Spec.Containers[i]
-				cpuUsage, err := ptptesthelper.GetContainerCpuUsage(pod.Name, container.Name, pod.Namespace, rateTimeWindow, prometheusPod)
-				if err != nil {
-					return false, fmt.Errorf("failed to get total cpu usage for ptp pods on node %s: %w", nodeName, err)
-				}
-
-				logrus.Infof("Node %s: pod: %s, container: %s (ns:%s) cpu usage: %.5f", nodeName, pod.Name, container.Name, pod.Namespace, cpuUsage)
 
 				// Should we check the total cpu usage for this container?
 				checkCpuUsage, cpuUsageThreshold := cpuTestConfig.ShouldCheckContainerCpuUsage(pod.Name, container.Name)
 				if !checkCpuUsage {
 					continue
+				}
+
+				cpuUsage, err := ptptesthelper.GetContainerCpuUsage(pod.Name, container.Name, pod.Namespace, rateTimeWindow, prometheusPod)
+				if err != nil {
+					return false, fmt.Errorf("failed to get total cpu usage for ptp pods on node %s: %w", nodeName, err)
 				}
 
 				logrus.Debugf("Checking cpu usage of container %s (pod %s). Cpu Usage: %.5f - Threshold: %.5f", container.Name, pod.Name, cpuUsage, cpuUsageThreshold)
