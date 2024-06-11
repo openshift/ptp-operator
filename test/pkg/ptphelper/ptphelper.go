45b21323b9220dae7a1fa6ce850e0646235f7ba5
Jack Ding <jackding@gmail.com>
2024-06-11 13:20:58 -0400

    ci test: fix message_tag matching with {level}
    Signed-off-by: Jack Ding <jackding@gmail.com>

--- test/pkg/ptphelper/ptphelper.go
+++ test/pkg/ptphelper/ptphelper.go
@@ -74,6 +74,10 @@ func GetClockIDMaster(ptpConfigName string, label *string, nodeName *string, isG
 	if err != nil {
 		return id, err
 	}
+	// 4.16+ has log levels in message_tag: [ptp4l.0.config:{level}]
+	if strings.Contains(logID, "level") {
+		logID = strings.Replace(logID, "{level}", "\\d+", 1)
+	}
 	ptpPods, err := client.Client.CoreV1().Pods(pkg.PtpLinuxDaemonNamespace).List(context.Background(), metav1.ListOptions{LabelSelector: "app=linuxptp-daemon"})
 	if err != nil {
 		return id, err
@@ -105,6 +109,10 @@ func GetClockIDForeign(ptpConfigName string, label *string, nodeName *string) (i
 	logID, err := GetProfileLogID(ptpConfigName, label, nodeName)
 	if err != nil {
 		return id, err
+	}
+	// 4.16+ has log levels in message_tag: [ptp4l.0.config:{level}]
+	if strings.Contains(logID, "level") {
+		logID = strings.Replace(logID, "{level}", "\\d+", 1)
 	}
 	ptpPods, err := client.Client.CoreV1().Pods(pkg.PtpLinuxDaemonNamespace).List(context.Background(), metav1.ListOptions{LabelSelector: "app=linuxptp-daemon"})
 	if err != nil {
