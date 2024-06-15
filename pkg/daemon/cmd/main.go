openshift/linuxptp-daemon

Add automatic leap file update
This adds automatic leap file updates from GPS data.

Signed-off-by: Vitaly Grinberg <v.g@redhat.com>

cmd/main.go
@@ -18,6 +18,7 @@ import (
 
 	"github.com/openshift/linuxptp-daemon/pkg/config"
 	"github.com/openshift/linuxptp-daemon/pkg/daemon"
+	"github.com/openshift/linuxptp-daemon/pkg/leap"
 	ptpv1 "github.com/openshift/ptp-operator/api/v1"
 	ptpclient "github.com/openshift/ptp-operator/pkg/client/clientset/versioned"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -109,6 +110,14 @@ func main() {
 	hwconfigs := []ptpv1.HwConfig{}
 	refreshNodePtpDevice := true
 	closeProcessManager := make(chan bool)
+	lm, err := leap.New(kubeClient, daemon.PtpNamespace)
+	if err != nil {
+		glog.Error("failed to initialize Leap manager, ", err)
+		return
+	}
+	go lm.Run()
+
+	defer close(lm.Close)
 	go daemon.New(
 		nodeName,
 		daemon.PtpNamespace,
@@ -121,6 +130,7 @@ func main() {
 		&refreshNodePtpDevice,
 		closeProcessManager,
 		cp.pmcPollInterval,
+		lm,
 	).Run()
 
 	tickerPull := time.NewTicker(time.Second * time.Duration(cp.updateInterval))

pkg/daemon/gpsd.go
@@ -13,6 +13,7 @@ import (
 	"github.com/golang/glog"
 	"github.com/openshift/linuxptp-daemon/pkg/config"
 	"github.com/openshift/linuxptp-daemon/pkg/event"
+	"github.com/openshift/linuxptp-daemon/pkg/leap"
 	"github.com/openshift/linuxptp-daemon/pkg/ublox"
 	gpsdlib "github.com/stratoberry/go-gpsd"
 )
@@ -43,6 +44,7 @@ type GPSD struct {
 	subscriber           *GPSDSubscriber
 	monitorCtx           context.Context
 	monitorCancel        context.CancelFunc
+	leapManager          *leap.LeapManager
 }
 
 // GPSDSubscriber ... event subscriber
@@ -198,6 +200,7 @@ func (g *GPSD) CmdRun(stdoutToSocket bool) {
 // MonitorGNSSEventsWithUblox ... monitor GNSS events with ublox
 func (g *GPSD) MonitorGNSSEventsWithUblox() {
 	//var ublx *ublox.UBlox
+	const timeLsResultLines = 4
 	g.state = event.PTP_FREERUN
 	ticker := time.NewTicker(GNSSMONITOR_INTERVAL)
 	doneFn := func() {
@@ -245,6 +248,16 @@ retry:
 						nStatus = ublox.ExtractNavStatus(nextLine)
 						emptyCount = 0
 						missedTickers = 0
+					} else if strings.Contains(output, "UBX-NAV-TIMELS") {
+						emptyCount = 0
+						missedTickers = 0
+						lines := []string{}
+						for i := 0; i < timeLsResultLines; i++ {
+							line := ublx.UbloxPollPull()
+							lines = append(lines, line)
+						}
+						timeLs := ublox.ExtractLeapSec(lines)
+						g.leapManager.UbloxLsInd <- *timeLs
 					} else if len(output) == 0 {
 						emptyCount++
 					}

openshift/ptp-operator

pkg/daemon/cmd/main.go
@@ -19,6 +19,7 @@ import (
 	ptpclient "github.com/k8snetworkplumbingwg/ptp-operator/pkg/client/clientset/versioned"
 	"github.com/k8snetworkplumbingwg/ptp-operator/pkg/daemon/config"
 	"github.com/k8snetworkplumbingwg/ptp-operator/pkg/daemon/daemon"
+	"github.com/k8snetworkplumbingwg/ptp-operator/pkg/leap"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 )
@@ -102,6 +103,14 @@ func main() {
 	hwconfigs := []ptpv1.HwConfig{}
 	refreshNodePtpDevice := true
 	closeProcessManager := make(chan bool)
+	lm, err := leap.New(kubeClient, daemon.PtpNamespace)
+	if err != nil {
+		glog.Error("failed to initialize Leap manager, ", err)
+		return
+	}
+	go lm.Run()
+
+	defer close(lm.Close)
 	go daemon.New(
 		nodeName,
 		daemon.PtpNamespace,
@@ -113,6 +122,7 @@ func main() {
 		&refreshNodePtpDevice,
 		closeProcessManager,
 		cp.pmcPollInterval,
+		lm,
 	).Run()
 
 	tickerPull := time.NewTicker(time.Second * time.Duration(cp.updateInterval))

pkg/daemon/daemon/gpsd.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/openshift/linuxptp-daemon/pkg/event"
 	"github.com/openshift/linuxptp-daemon/pkg/gnss"
+	"github.com/k8snetworkplumbingwg/ptp-operator/pkg/leap"
 	gpsdlib "github.com/stratoberry/go-gpsd"
 )
 
@@ -41,6 +42,7 @@ type GPSD struct {
 	subscriber           *GPSDSubscriber
 	monitorCtx           context.Context
 	monitorCancel        context.CancelFunc
+	leapManager          *leap.LeapManager
 }
 
 // GPSDSubscriber ... event subscriber
@@ -196,6 +198,7 @@ func (g *GPSD) CmdRun(stdoutToSocket bool) {
 // MonitorGNSSEventsWithUblox ... monitor GNSS events with ublox
 func (g *GPSD) MonitorGNSSEventsWithUblox() {
 	//var ublx *ublox.UBlox
+	const timeLsResultLines = 4
 	g.state = event.PTP_FREERUN
 	ticker := time.NewTicker(GNSSMONITOR_INTERVAL)
 	doneFn := func() {
@@ -243,6 +246,16 @@ retry:
 						nStatus = ublox.ExtractNavStatus(nextLine)
 						emptyCount = 0
 						missedTickers = 0
+					} else if strings.Contains(output, "UBX-NAV-TIMELS") {
+						emptyCount = 0
+						missedTickers = 0
+						lines := []string{}
+						for i := 0; i < timeLsResultLines; i++ {
+							line := ublx.UbloxPollPull()
+							lines = append(lines, line)
+						}
+						timeLs := ublox.ExtractLeapSec(lines)
+						g.leapManager.UbloxLsInd <- *timeLs
 					} else if len(output) == 0 {
 						emptyCount++
 					}