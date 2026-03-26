67b2b19f44fed46607522edc595efd0b758e6ba5
Jack Ding <jackding@gmail.com>
2026-03-26 18:35:20 -0400

    Add TLS adherence support
    Read the tlsAdherence policy from the APIServer CR and use
    ShouldHonorClusterTLSProfile to conditionally apply the cluster
    TLS profile. In Legacy mode (default), Go TLS defaults are used.
    In Strict mode, the cluster-wide TLS profile is enforced on the
    operator's webhook/metrics servers and on the daemon's
    kube-rbac-proxy.
    
    The SecurityProfileWatcher now also monitors adherence policy
    changes and triggers a graceful restart when the policy changes.
    
    Signed-off-by: Jack Ding <jackding@gmail.com>

--- controllers/tls_profile_test.go
+++ controllers/tls_profile_test.go
@@ -61,14 +61,8 @@ func TestTLSProfileTemplateData(t *testing.T) {
 	}
 }
 
-func TestTLSProfileTemplateRendering(t *testing.T) {
-	profile := *configv1.TLSProfiles[configv1.TLSProfileIntermediateType]
-	ianaCiphers := libgocrypto.OpenSSLToIANACipherSuites(profile.Ciphers)
-
+func makeTestRenderData() *render.RenderData {
 	data := render.MakeRenderData()
-	data.Data["TLSMinVersion"] = string(profile.MinTLSVersion)
-	data.Data["TLSCipherSuites"] = strings.Join(ianaCiphers, ",")
-	// Set all required template variables
 	data.Data["Image"] = "test-image"
 	data.Data["ImagePullPolicy"] = "IfNotPresent"
 	data.Data["Namespace"] = "openshift-ptp"
@@ -80,8 +74,18 @@ func TestTLSProfileTemplateRendering(t *testing.T) {
 	data.Data["EnabledPlugins"] = "e810"
 	data.Data["StorageType"] = "emptyDir"
 	data.Data["EventApiVersion"] = "2.0"
+	return &data
+}
 
-	objs, err := render.RenderTemplate("../bindata/linuxptp/ptp-daemon.yaml", &data)
+func TestTLSProfileTemplateRendering(t *testing.T) {
+	profile := *configv1.TLSProfiles[configv1.TLSProfileIntermediateType]
+	ianaCiphers := libgocrypto.OpenSSLToIANACipherSuites(profile.Ciphers)
+
+	data := makeTestRenderData()
+	data.Data["TLSMinVersion"] = string(profile.MinTLSVersion)
+	data.Data["TLSCipherSuites"] = strings.Join(ianaCiphers, ",")
+
+	objs, err := render.RenderTemplate("../bindata/linuxptp/ptp-daemon.yaml", data)
 	assert.NoError(t, err)
 	assert.NotEmpty(t, objs)
 
@@ -119,6 +123,54 @@ func TestTLSProfileTemplateRendering(t *testing.T) {
 		assert.Contains(t, rbacProxyArgs, expectedMinVersion)
 	}
 	assert.True(t, dsFound, "DaemonSet not found in rendered objects")
+}
+
+func TestTLSProfileTemplateRendering_LegacyAdherence(t *testing.T) {
+	// When TLS adherence is legacy, kube-rbac-proxy should use the hardcoded
+	// cipher suites that were in place before cluster TLS profile support.
+	data := makeTestRenderData()
+	data.Data["TLSMinVersion"] = ""
+	data.Data["TLSCipherSuites"] = "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256," +
+		"TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256," +
+		"TLS_RSA_WITH_AES_128_CBC_SHA256," +
+		"TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256," +
+		"TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256"
+
+	objs, err := render.RenderTemplate("../bindata/linuxptp/ptp-daemon.yaml", data)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, objs)
+
+	for _, obj := range objs {
+		if obj.GetKind() != "DaemonSet" {
+			continue
+		}
+		containers, found, err := unstructuredContainers(obj.Object)
+		assert.NoError(t, err)
+		assert.True(t, found)
+
+		var rbacProxyArgs []string
+		for _, c := range containers {
+			container := c.(map[string]interface{})
+			if container["name"] == "kube-rbac-proxy" {
+				args := container["args"].([]interface{})
+				for _, a := range args {
+					rbacProxyArgs = append(rbacProxyArgs, a.(string))
+				}
+				break
+			}
+		}
+
+		for _, arg := range rbacProxyArgs {
+			assert.NotContains(t, arg, "--tls-min-version",
+				"TLS min version should not be set in legacy adherence mode")
+		}
+		assert.Contains(t, rbacProxyArgs,
+			"--tls-cipher-suites=TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,"+
+				"TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,"+
+				"TLS_RSA_WITH_AES_128_CBC_SHA256,"+
+				"TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,"+
+				"TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256")
+	}
 }
 
 func unstructuredContainers(obj map[string]interface{}) ([]interface{}, bool, error) {
